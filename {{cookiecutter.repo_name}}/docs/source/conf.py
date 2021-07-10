d3f9a4b59f34e1c1946497045efcb143c6233579
Ben Cunningham <benjamescunningham@gmail.com>
2021-07-10 19:20:02 -0400

    Setup Sphinx conf.py

---
+++ {{cookiecutter.repo_name}}/docs/source/conf.py
@@ -0,0 +1,26 @@
+import datetime
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath("../../src"))
+sys.path.append(os.path.abspath("./demo/"))
+
+project = "{{ cookiecutter.repo_name }}"
+copyright = "{}, {}. All rights reserved.".format(datetime.datetime.now().year)
+author = "{{ cookiecutter.author_name }}"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_multiversion",
+    "{{ cookiecutter.import_name }}",
+]
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "{{ cookiecutter.import_name }}"
+
+smv_branch_whitelist = r"^(master|develop|release\/.*)$"
+smv_tag_whitelist = r"^v\d+\.\d+\.\d+$"
+smv_released_pattern = r"^.*(tags|master).*$"
+smv_remote_whitelist = r"^(origin)$"
